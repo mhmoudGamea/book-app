fe6932391f894a5eb55545016a43307e3f44e025
Mhmoud gamea <mhmoudgamea@gmail.com>
2023-02-02 12:41:36 +0200

    add custom scroll view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -13,38 +13,46 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
-      child: Column(
-        children: [
-          const SizedBox(height: 5),
-          const CustomBookDetailsAppBar(),
-          const SizedBox(height: 20),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.25),
-            child: const BookCover(),
-          ),
-          const SizedBox(height: 30),
-          const Text('The Jungle Book', style: Styles.header, maxLines: 2),
-          const SizedBox(height: 3),
-          const Text('Rudyard Kipling', style: Styles.description),
-          const SizedBox(height: 3),
-          const RatingBox(),
-          const SizedBox(height: 30),
-          const CustomBoxButton(),
-          const SizedBox(height: 20),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.titleMedium
-                  .copyWith(fontWeight: FontWeight.w500, fontSize: 17),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+            child: Column(
+              children: [
+                const SizedBox(height: 5),
+                const CustomBookDetailsAppBar(),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * 0.25),
+                  child: const BookCover(),
+                ),
+                const SizedBox(height: 30),
+                const Text('The Jungle Book',
+                    style: Styles.header, maxLines: 2),
+                const SizedBox(height: 3),
+                const Text('Rudyard Kipling', style: Styles.description),
+                const SizedBox(height: 3),
+                const RatingBox(),
+                const SizedBox(height: 30),
+                const CustomBoxButton(),
+                const Expanded(child: SizedBox(height: 20)),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.titleMedium
+                        .copyWith(fontWeight: FontWeight.w500, fontSize: 17),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const CustomListView(fraction: 0.15),
+                const SizedBox(height: 20),
+              ],
             ),
           ),
-          const SizedBox(height: 10),
-          const CustomListView(fraction: 0.15),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
